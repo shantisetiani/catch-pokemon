c8111a91996e9d02aefe6fe68d368a7d1886ce03
shantisetiani <shantisetiani01@gmail.com>
2021-03-25 14:52:17 +0700

    Updates

---
+++ precache-manifest.c6346b94bac029bf4f33e1546dd1cc28.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2b2af01a175a6fe2c336b54144772f",
+    "url": "/catch-pokemon/index.html"
+  },
+  {
+    "revision": "c408c8717ffa21def2cd",
+    "url": "/catch-pokemon/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "54de32f8ce19c007f688",
+    "url": "/catch-pokemon/static/css/main.03d0a456.chunk.css"
+  },
+  {
+    "revision": "c408c8717ffa21def2cd",
+    "url": "/catch-pokemon/static/js/2.fdb90f42.chunk.js"
+  },
+  {
+    "revision": "55252f66194a9a62463d161bd7a0d470",
+    "url": "/catch-pokemon/static/js/2.fdb90f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54de32f8ce19c007f688",
+    "url": "/catch-pokemon/static/js/main.57f92841.chunk.js"
+  },
+  {
+    "revision": "988b499a60ebccb7779c",
+    "url": "/catch-pokemon/static/js/runtime-main.5d6e5c93.js"
+  },
+  {
+    "revision": "085f09847cb32c99c6870e8c82809cee",
+    "url": "/catch-pokemon/static/media/no-image.085f0984.jpg"
+  },
+  {
+    "revision": "bc656300bed1f928413d44a097049c8f",
+    "url": "/catch-pokemon/static/media/pokeball.bc656300.gif"
+  },
+  {
+    "revision": "023d617a1b733e2862bc8d439cface20",
+    "url": "/catch-pokemon/static/media/pokemon-logo.023d617a.png"
+  }
+]);
